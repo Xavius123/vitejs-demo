62ff6102321451a3cc8272f0403bfa537120cfc5
Dan.Weigel <dan.weigel@nafinc.com>
2025-01-10 13:20:04 -0800

    added a few items

---
+++ src/app/test/test.component.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from 'react';
+
+export const TestComponent = (): ReactElement => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const TestComponent = '';
+
+    return <div>TEST Component</div>;
+};
