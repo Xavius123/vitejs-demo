1613d94db4d3ea22ba5bbd62ef662a71e07f2320
Xavius123 <dantweigel@gmail.com>
2025-01-12 02:51:41 -0600

    added script options

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// load env options
+const env = loadEnv(mode, process.cwd(), '');
+// scripts to add 
+// npm run dev -- --mode development
+// npm run build -- --mode qa
+// npm run build -- --mode uat
+// npm run build -- --mode staging
+// npm run build -- --mode production
+
+// preview build script
+//  "preview": "vite preview --port 8080"
 
 export default defineConfig({
     plugins: [react()],
@@ -19,6 +31,9 @@ export default defineConfig({
     //     },
     //     preprocessorOptions: { scss: { additionalData: `@import "./src/main/variables.scss";` } },
     // },
+    define: {
+        'process.env': env, // Inject environment variables
+      },
     server: {
         port: 3000,
     },
